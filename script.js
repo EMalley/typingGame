f4be7cd4289a1bbea70b8e7922e838ce2f3ba861
EMalley <eric.malley.dev@gmail.com>
2025-04-01 13:58:52 -0400

    game logic completed.

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ let words = [];
 let wordIndex = 0;
 let startTime = Date.now();
 
-const quoteElement = document.getElementById("quotes");
-const message = document.getElementById("message");
-const typedValue = document.getElementById("typedValue");
+const quoteElement = document.getElementById("quoteElement");
+const messageElement = document.getElementById("messageElement");
+const typedValueElement = document.getElementById("typedValueElement");
 const start = document.getElementById("start");
 
 start.addEventListener("click", () => {
@@ -26,12 +26,39 @@ start.addEventListener("click", () => {
 
   //UI updates
   const spanWords = words.map(function (word) {
-    return `<span>${word}</span>`;
+    return `<span>${word} </span>`;
   });
-  quoteElement.innerHTML = spanWords;
+  quoteElement.innerHTML = spanWords.join("");
   quoteElement.childNodes[0].className = "highlight";
-  message.innerText = "";
+  messageElement.innerText = "";
 
-  typedValue.focus();
+  typedValueElement.focus();
   startTime = new Date().getTime();
+});
+
+typedValueElement.addEventListener("input", () => {
+  const currentWord = words[wordIndex];
+  const typedValue = typedValueElement.value;
+
+  if (typedValue === currentWord && wordIndex === words.length - 1) {
+    const elapsedTime = new Date.getTime() - startTime;
+    const message = `Congradulations! you finished typing in ${
+      elapsedTime / 1000
+    } seconds`;
+    messageElement.innerHTML(message);
+  } else if (typedValue.endsWith(" ") && typedValue.trim() === currentWord) {
+    typedValueElement.value = " ";
+    wordIndex++;
+    //reset classname for elements in the quote
+    for (const wordElement of quoteElement.childNodes) {
+      wordElement.className = "";
+    }
+    //highlight the new word
+    quoteElement.childNodes[wordIndex].className = "highlight";
+  } else if (currentWord.startsWith(typedValue)) {
+    typedValueElement.className = "";
+  } else {
+    // set error state
+    typedValueElement.className = "error";
+  }
 });
